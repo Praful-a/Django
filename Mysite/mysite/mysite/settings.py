0228b18f1984465c4de89470563533c658256ff8
Praful-a <prafuldubey27@gmail.com>
2021-07-05 18:11:31 +0530

    work with database

--- Mysite/mysite/mysite/settings.py
+++ Mysite/mysite/mysite/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Calcutta'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
